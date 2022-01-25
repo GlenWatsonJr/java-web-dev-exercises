7b201202e55dd843fad96913d34be7efc5cb0304
Glen Watson <glenwatsonjr@gmail.com>
2022-01-25 15:39:08 -0500

    Added the bomus mission

--- src/org/launchcode/java/studios/areaofacircle/Area.java
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -4,11 +4,23 @@ import java.util.Scanner;
 
 public class Area {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("What is the radius? ");
-        double radius = input.nextDouble();
-        double area = Circle.getArea(radius);
-        System.out.println("The area of a circle of radius " + radius + " is: " + area);
 
+        boolean notCorrectInput = true;
+        while (notCorrectInput) {
+            Scanner input = new Scanner(System.in);
+            System.out.println("What is the radius? ");
+            if (input.hasNextDouble()) {
+                double radius = input.nextDouble();
+                if (radius > 0) {
+                    double area = Circle.getArea(radius);
+                    System.out.println("The area of a circle of radius " + radius + " is: " + area);
+                    notCorrectInput = false;
+                } else {
+                    System.out.println("You entered a negative integer ");
+                }
+            } else {
+                System.out.println("Please Enter Valid Integer");
+            }
+        }
     }
 }
